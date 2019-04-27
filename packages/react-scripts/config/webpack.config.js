1c0738b0665b3d96c673b1f171a172d240a65445
Dinislam Tebuev <dinislam@curebase.com>
2019-04-26 17:54:27 -0700

    fixes

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -417,19 +417,6 @@ module.exports = function(webpackEnv) {
                     'react-scripts',
                   ]
                 ),
-                // @remove-on-eject-end
-                plugins: [
-                  [
-                    require.resolve('babel-plugin-named-asset-import'),
-                    {
-                      loaderMap: {
-                        svg: {
-                          ReactComponent: '@svgr/webpack?-svgo,+ref![path]',
-                        },
-                      },
-                    },
-                  ],
-                ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
                 // directory for faster rebuilds.
